8dd277c3ef7cfc507d241bb2152e8ce4927dbed8
fahmifan <fahmiirfan909@gmail.com>
2018-04-16 23:08:56 +0700

    Add class Keys for intent keys

---
+++ app/src/main/java/com/syifa/a140810160026/Keys.java
@@ -0,0 +1,12 @@
+package com.syifa.a140810160026;
+
+/**
+ * Constant keys for passing intent*/
+public class Keys {
+    public  static final String SATUAN = "SATUAN";
+    public static final  String VAL = "VAL";
+    public static final String CELCIUS = "CELCIUS";
+    public static final String REAMUR = "REAMUR";
+    public static final String KELVIN = "KELVIN";
+    public static final String FAHRENHEIT = "FAHRENHEIT";
+}
