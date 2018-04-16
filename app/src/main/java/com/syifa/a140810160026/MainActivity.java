19dc74922dd822e2194d120b3bbaa233632d7c7c
fahmifan <fahmiirfan909@gmail.com>
2018-04-16 23:07:29 +0700

    fix MainActivity

--- app/src/main/java/com/syifa/a140810160026/MainActivity.java
+++ app/src/main/java/com/syifa/a140810160026/MainActivity.java
@@ -4,47 +4,62 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private String celcius = "Celcius : ";
-    private String Reamur = "Reamur : ";
-    private String Farenheit = "Farenheit : ";
-    private String Kelvin = "Kelvin : ";
-
+    private double suhu;
+    private String satuan;
+    private String getSuhu;
+    private String getSatuan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClikKonversi(View view){
-        try {
-            EditText getSuhu = (EditText) findViewById(R.id.editSuhu);
-            EditText getSatuan = (EditText) findViewById(R.id.editSatuan);
-            String suhu = getSuhu.getText().toString();
-            String satuan = getSatuan.getText().toString();
-            if (! suhu.isEmpty() || ! satuan.isEmpty()){
-                switch (satuan){
-                    case satuan="celcius":
-                        break;
-                    case satuan="reamur":
-                        break;
-                    case satuan="farenheit":
-                        break;
-                    case satuan="kelvin":
-                        break;
-                    default:
-                        break;
+        Button btnOk = findViewById(R.id.button_ok);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSuhu = ((EditText) findViewById(R.id.inputSuhu)).getText().toString();
+                getSatuan = ((EditText) findViewById(R.id.inputSatuan)).getText().toString();
+
+                if (getSuhu.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Required Suhu", Toast.LENGTH_SHORT).show();
+                }
+                if (getSatuan.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Required Satuan", Toast.LENGTH_SHORT).show();
+                } else {
+                    suhu = Double.parseDouble(getSuhu);
+                    satuan = getSatuan.toUpperCase();
+                    onClickKonversi();
                 }
             }
+        });
+    }
+
+    private void onClickKonversi() {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        intent.putExtra(Keys.VAL, this.suhu);
+
+        switch (this.satuan) {
+            case Keys.CELCIUS:
+                intent.putExtra(Keys.SATUAN, Keys.CELCIUS);
+                break;
+            case Keys.REAMUR:
+                intent.putExtra(Keys.SATUAN, Keys.REAMUR);
+                break;
+            case Keys.KELVIN:
+                intent.putExtra(Keys.SATUAN, Keys.KELVIN);
+                break;
+            case Keys.FAHRENHEIT:
+                intent.putExtra(Keys.SATUAN, Keys.FAHRENHEIT);
+            default:
+                break;
         }
+        startActivity(intent);
     }
-
-    public void openAvtivity2(){
-        Intent i = new Intent(MainActivity.this, Main2Activity.class);
-        startActivity(i);
-    }
-
 }
